9c6d40f95533b9dcc9958095b7f1f3f587639503
Andy Gonzalez <aglez9538@gmail.com>
2018-06-20 13:22:14 -0400

    Added BinaryTree definition

---
+++ FSharpDemo/FSharpDemo/BinaryTree.fs
@@ -0,0 +1,12 @@
+﻿module BinaryTree
+
+type BinaryTree<'a when 'a : equality and 'a : comparison>=
+    | Leaf of 'a
+    | Node of 'a * 'a BinaryTree * 'a BinaryTree
+    with
+        member this.Contains value = 
+                match this with
+                    | Leaf v -> v = value
+                    | Node(v, _, _) when v = value -> true
+                    | Node(v, left, _) when v > value -> left.Contains(value)
+                    | Node(_, _, right) -> right.Contains(value)
