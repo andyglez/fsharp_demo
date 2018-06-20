ba652d764f4c30ad0a0dfccd2b7575aa61124345
Andy Gonzalez <aglez9538@gmail.com>
2018-06-20 16:10:59 -0400

    Added some member for BinaryTree

--- FSharpDemo/FSharpDemo/BinaryTree.fs
+++ FSharpDemo/FSharpDemo/BinaryTree.fs
@@ -1,12 +1,28 @@
 ﻿module BinaryTree
 
-type BinaryTree<'a when 'a : equality and 'a : comparison>=
+type BinaryTree<'a when 'a : equality and 'a : comparison> =
+    | Nil
     | Leaf of 'a
     | Node of 'a * 'a BinaryTree * 'a BinaryTree
     with
         member this.Contains value = 
                 match this with
+                    | Nil -> false
                     | Leaf v -> v = value
                     | Node(v, _, _) when v = value -> true
-                    | Node(v, left, _) when v > value -> left.Contains(value)
-                    | Node(_, _, right) -> right.Contains(value)
+                    | Node(v, left, _) when v > value -> left.Contains value
+                    | Node(_, _, right) -> right.Contains value
+        member this.Add value =
+                match this with
+                    | Nil -> Leaf value
+                    | Leaf v when v = value -> Leaf v
+                    | Leaf v when v > value -> Node (v, Leaf value, Nil)
+                    | Leaf v when v < value -> Node (v, Nil, Leaf value)
+                    | Node(v, left, right) when v = value -> Node(v, left, right)
+                    | Node(v, left, right) when v > value -> Node(v, left.Add value, right)
+                    | Node(v, left, right) when v < value -> Node(v, left, right.Add value)
+        member this.InOrder =
+                match this with
+                    | Nil -> []
+                    | Leaf v -> [v]
+                    | Node(v, l, r) -> l.InOrder@[v]@r.InOrder
--- FSharpDemo/FSharpDemo/Program.fs
+++ FSharpDemo/FSharpDemo/Program.fs
@@ -1,7 +1,9 @@
 ﻿// Learn more about F# at http://fsharp.org
 // See the 'F# Tutorial' project for more help.
+open BinaryTree
 
 [<EntryPoint>]
 let main argv = 
-    printfn "%A" argv
+    let tree = Node (10, Leaf 5, Leaf 15)
+    printfn "%A" tree.InOrder
     0 // return an integer exit code
