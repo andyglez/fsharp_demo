089ed2fa2f8a047ba5fd2fe4b245b8df48d1ab54
Andy Gonzalez <aglez9538@gmail.com>
2018-06-20 19:46:22 -0400

    Added permutations implementation

--- FSharpDemo/FSharpDemo/Others.fs
+++ FSharpDemo/FSharpDemo/Others.fs
@@ -1,6 +1,25 @@
 ﻿module Others
 
+open Microsoft.FSharp.Linq
+
 let rec (!) n = 
     match n with
-    | 1 -> 1
-    | n -> n * !(n - 1)
+    | 1 -> bigint(1)
+    | n -> bigint(n) * !(n - 1)
+
+
+let rec remove x xs =
+    match xs with
+    | [] -> []
+    | (x'::xs') -> if x = x' then xs' else x'::(remove x xs')
+
+let rec permutations list = 
+    seq {   match list with
+            | [] -> yield []
+            | _ -> for item in list do
+                        for perm in permutations (Seq.toList(query {for x in list do
+                                                                        where (not (item.Equals(x)))
+                                                                        select x})) do//(List.where (fun x -> not (item.Equals(x))) list) do
+                            yield item::perm
+            }
+
--- FSharpDemo/FSharpDemo/Program.fs
+++ FSharpDemo/FSharpDemo/Program.fs
@@ -1,9 +1,15 @@
 ﻿// Learn more about F# at http://fsharp.org
 // See the 'F# Tutorial' project for more help.
 open BinaryTree
+open Others
 
 [<EntryPoint>]
 let main argv = 
     let tree = Node (10, Leaf 5, Leaf 15)
     printfn "%A" (tree.Add 20).InOrder
+
+    let l = lazy !1000000
+    printfn "%A" l
+
+    printfn "%A" (permutations [1; 2; 3])
     0 // return an integer exit code
